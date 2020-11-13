6a75aa8445d77feaa00d858f7f57f74c66df83b7
Jay <you@example.com>
2020-11-13 20:44:38 +0000

    Small styling changes and new logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
+import logo from './github-logo.png';
 import './App.css';
 import axios from 'axios';
 import React, {Component} from 'react';
-import { render } from 'react-dom';
+//import { render } from 'react-dom';
 
 class App extends Component{
   render() 
@@ -15,6 +15,8 @@ class App extends Component{
             GitHub Stats
           </h1>
           <h5>Coursework 2 - COMP3211 Distributed Systems</h5>
+          <li>This will give you some detailed statistics of a repository and all the users who have contributed to that repository. </li>
+          <li>This will generate a report that will be in the form of a PDF that you will be able to view in a browser or locally if you choose to download it</li>
           <label>Owner of Github Repository</label>
           <input placeholder="e.g octocat"></input>
           <label>Name of Repository</label>
