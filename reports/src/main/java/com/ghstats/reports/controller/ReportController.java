bed69de3af5645d8b8c94c5d3b93691fcdb51e6c
Igne Stalnionyte <igne.stal@gmail.com>
2020-11-14 20:03:01 +0000

    start implementing reports

--- reports/src/main/java/com/ghstats/reports/controller/ReportController.java
+++ reports/src/main/java/com/ghstats/reports/controller/ReportController.java
@@ -1,4 +1,34 @@
 package com.ghstats.reports.controller;
 
+import com.ghstats.reports.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class ReportController {
+
+    private static final String reportLink = "reports/src/main/resources/%s.pdf";
+
+    private final ReportService reportService;
+
+    @PostMapping(path = "/pdf")
+    private ResponseEntity<String> getReportLink() {
+        String repoName = "example-repo";
+        String link = String.format(reportLink, repoName);
+        try {
+            reportService.generatePdf(link, repoName);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.toString());
+        }
+        return ResponseEntity.ok(link);
+    }
 }
--- reports/src/main/java/com/ghstats/reports/service/ReportService.java
+++ reports/src/main/java/com/ghstats/reports/service/ReportService.java
@@ -1,4 +1,88 @@
 package com.ghstats.reports.service;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
 public class ReportService {
+
+    private static final float startX = 70;
+    private static final float startY = 700;
+
+    private static final PDFont DEFAULT_FONT = PDType1Font.COURIER;
+
+    private PDDocument document;
+    private PDPage page;
+    private PDPageContentStream contentStream;
+
+    public void generatePdf(String location, String repoName) throws IOException {
+        document = new PDDocument();
+        page = new PDPage();
+        contentStream = new PDPageContentStream(document, page);
+
+        document.addPage(page);
+
+        contentStream.beginText();
+        writeHeader("Statistics for GitHub repository " + repoName);
+        writeRepoStatistics();
+        writeContributorStatistics();
+
+        contentStream.endText();
+        contentStream.close();
+        document.save(location);
+        document.close();
+    }
+
+    private void writeNewLineAtCoordinates(PDFont font, int fontSize, String content, float x, float y) throws IOException {
+        contentStream.setFont(font, fontSize);
+        contentStream.newLineAtOffset(x, y);
+        contentStream.showText(content);
+    }
+
+    private void writeNewLine(PDFont font, int fontSize, String content) throws IOException {
+        contentStream.setFont(font, fontSize);
+        contentStream.newLine();
+        contentStream.showText(content);
+    }
+
+    private void writeHeader(String content) throws IOException {
+        contentStream.setLeading(14.5f);
+        writeNewLineAtCoordinates(PDType1Font.COURIER_BOLD, 18, content, startX, startY);
+    }
+
+    private void writeRepoStatistics() throws IOException {
+        String header = "Repository statistics";
+        writeNewLine(DEFAULT_FONT, 16, header);
+        contentStream.newLine();
+        contentStream.setFont(DEFAULT_FONT, 12);
+        contentStream.showText("Total commits: " + 10);
+        contentStream.newLine();
+        contentStream.showText("First commit: " + LocalDate.now().toString());
+        contentStream.newLine();
+        contentStream.showText("Latest commit: " + LocalDate.now().toString());
+        contentStream.newLine();
+        contentStream.showText("Activity duration: " + 10 + " days");
+        contentStream.newLine();
+    }
+
+    private void writeContributorStatistics() throws IOException {
+        String header = "Contributor statistics";
+        writeNewLine(DEFAULT_FONT, 16, header);
+        contentStream.newLine();
+        contentStream.setFont(DEFAULT_FONT, 14);
+        contentStream.showText("1. iigne ");
+        contentStream.newLine();
+
+        contentStream.showText("2. shimmamconyx");
+        contentStream.newLine();
+    }
+
 }
