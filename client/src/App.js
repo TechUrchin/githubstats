984caa3b88a9473dcb03c8939c34d19fd730d69f
Jay Pluckrose <ryumconyx@hotmail.co.uk>
2020-11-16 18:04:04 +0000

    Posts to Stats and returns results as intended

--- client/src/App.js
+++ client/src/App.js
@@ -26,30 +26,108 @@ class App extends Component{
       </div>
     );
   }
-
+state = {
+  repository: {}
+}
   getGitHub = () => {
     //config = {headers: {Authorization: `Bearer 4dc896d320882759be824f64df3e1afa4675857b`}
+    let repository
     axios.get(
-      `https://api.github.com/repos/octocat/hello-world`
+      `https://api.github.com/repos/octocat/hello-world/commits`
       ).then(res => {
-      const repo = res.data;
-      console.log("hello from GH:", repo)
+      const commits = res.data;
+      console.log("hello from GH:", commits);
+      repository = this.formatCommit(commits);
+      this.setState({repository}); 
+      this.postToStats();
       });
+
     //call stats
-    axios.get(
-        '/api/stats/hello'
-    )
-        .then(res => {
-          console.log("Hello from stats api:", res.data)
-        });
+    
+
     //call reports
-    axios.get(
+    /*axios.get(
         '/api/reports/hello'
     )
         .then(res => {
           console.log("Hello from reports api:", res.data)
         });
+    */
 
+  }
+
+  postToStats = () => {
+    let stats
+    console.log("State", this.state)
+    axios.post(
+        '/api/stats/generate', this.state.repository, {
+          "Content-Type":"application/json"
+        }
+    )
+        .then(res => {
+          stats = res.data;
+          console.log(stats)
+        });
+    this.postToReport();
+  }
+
+  postToReport = () => {
+    axios.post(
+      '/api/stats/generate', this.state.stats, {
+        "Content-Type":"application/json"
+      }
+  )
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+  }
+
+
+  formatCommit = (commits) => {
+    console.log("hello from formatCommit ", commits)
+    //Initialises empty JSON Object
+    let repository = {
+      "repositoryName": "",
+      "repositoryOwner": "",
+      "commits": []
+    }
+
+    let commitObject = {
+      "commitDate": "",
+          "author": 
+          {
+            "gitName": "",
+            "gitEmail": "",
+            "githubUsername": "",
+            "githubUserIcon": ""
+          }
+    }
+
+    repository.repositoryName = "Hello-World"; //variable needs to be defined
+    repository.repositoryOwner = "Octocat"; //Same here
+    commits.forEach(i =>
+    {
+      console.log(i)
+      commitObject.commitDate = i.commit.author.date;
+      if(i.author == null)
+      {
+        commitObject.author.gitName = i.commit.author.name;
+        commitObject.author.gitEmail = i.commit.author.email;
+        commitObject.author.githubUsername = null;
+        commitObject.author.githubUserIcon = null;
+      }
+      else{
+        commitObject.author.gitName = i.commit.author.name;
+        commitObject.author.gitEmail = i.commit.author.email;
+        commitObject.author.githubUsername = i.author.login;
+        commitObject.author.githubUserIcon = i.author.avatar_url;
+      }
+      const updatedCommit = JSON.parse(JSON.stringify(commitObject));
+      repository.commits.push(updatedCommit);
+    })
+    console.log("All the Commits", repository)
+    return repository;
   }
 
 }
