3e3f12947658f7738af456474f2adb48c90922e8
Igne Stalnionyte <igne.stal@gmail.com>
2020-11-18 17:45:34 +0000

    redo the client to run under the statistics server

--- reports/src/main/java/com/ghstats/reports/ReportsApp.java
+++ reports/src/main/java/com/ghstats/reports/ReportsApp.java
@@ -2,10 +2,23 @@ package com.ghstats.reports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ReportsApp {
     public static void main( String[] args ) {
         SpringApplication.run(ReportsApp.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/reports/pdf").allowedOrigins("http://localhost:8000");
+            }
+        };
     }
 }
